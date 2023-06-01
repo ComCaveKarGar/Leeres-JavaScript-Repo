7ebd3605410dd86b34e2e6107211095df0a77a16
Karsten <Moonjumper@gmx.de>
2023-06-01 16:12:21 +0200

    +1

--- script_01.js
+++ script_01.js
@@ -1,3 +1,26 @@
-
-
-
+function rechner(num1, num2, operation) {
+    var ergebnis;
+    
+    if (operation === "summe") {
+      ergebnis = summe(num1, num2);
+    } else if (operation === "differenz") {
+      ergebnis = differenz(num1, num2);
+    } else {
+      console.log("geht nicht!");
+      return;
+    }
+    
+    console.log("Das Ergebnis lautet: " + ergebnis);
+  }
+  
+  function summe(a, b) {
+    return a + b;
+  }
+  
+  function differenz(a, b) {
+    return a - b;
+  }
+  
+  
+  rechner(5, 3, "summe"); // Das Ergebnis lautet: 8
+  rechner(10, 6, "differenz"); // Das Ergebnis lautet: 4
